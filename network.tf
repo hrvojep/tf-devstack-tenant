3c8a840773d8d16db418f2c5fa391557f1a7f4db
pandom <pandom@users.noreply.github.com>
2021-06-18 11:33:27 +1000

    address

--- network.tf
+++ network.tf
@@ -22,6 +22,6 @@ resource "openstack_networking_floatingip_v2" "fip_1" {
 }
 
 resource "openstack_networking_floatingip_associate_v2" "fip_1" {
-  floating_ip = openstack_networking_floatingip_v2.fip_1.floating_ip
+  floating_ip = openstack_networking_floatingip_v2.fip_1.address
   port_id = openstack_networking_port_v2.port_1.id
 }
